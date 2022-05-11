4b328ba12a6470f83c6757d5a9d53698b5ce049e
José Duque <duquejo01@gmail.com>
2022-05-10 22:12:35 -0500

    Custom fonts

--- src/components/Layouts/MainContainer.tsx
+++ src/components/Layouts/MainContainer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const MainContainer = (): JSX.Element | null => {
   return (
     <div className="bg-slate-100 w-2/3 h-1/2 p-6 rounded-xl shadow-xl">
-      <h1 className="text-4xl font-bold pb-3">Hello world!</h1>
+      <h1 className="text-7xl font-bold pb-3">Hello world!</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet saepe id ad a ullam ipsum illo rerum fuga provident quas?</p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      Dosis: [ "Dosis", "sans-serif" ]
+    },
+    fontFamily: {
+      display: [ 'Dosis' ],
+      body: [ 'Dosis' ],
+    },
   },
   plugins: [],
 }
