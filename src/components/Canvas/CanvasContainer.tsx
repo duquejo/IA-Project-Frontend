44cb4137bd6e59963c3af2b16b68034b1189dc95
José Duque <duquejo01@gmail.com>
2022-05-14 01:18:59 -0500

    feat: General Layout and some calculations

--- src/components/Canvas/CanvasContainer.tsx
+++ src/components/Canvas/CanvasContainer.tsx
@@ -1,28 +1,56 @@
-import React, { useState } from 'react';
-import CanvasDraw from 'react-canvas-draw';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import CanvasDraw, { CanvasDrawProps } from 'react-canvas-draw';
 
-interface ICanvasContainer {
-  width: number;
-  height: number;
-}
+// interface ICanvasContainer {
+//   width: number;
+//   height: number;
+// }
 
-export const CanvasContainer: React.FC<ICanvasContainer> = ({ width, height }): JSX.Element | null => {
+export const CanvasContainer: React.FC<any> = (): JSX.Element | null => {
 
-  console.log( width, height );
+  const currentCanvas = useRef<any>(null);
 
-  const [canvas,_] = useState({
-    canvasWidth: width,
-    canvasHeight: height,
-    brushRadius: 10,
-    lazyRadius: 15,
-    hideGrid: true,
+  const [canvas,_] = useState<any>({
+    canvasWidth: 500,
+    canvasHeight: 500,
+    brushRadius: 7,
+    lazyRadius: 0,
     gridColor: '#FFFFFF',
-    className: 'canvas'
   });
 
+  const handleEraseCanvas = () => {
+    currentCanvas.current.clear();
+  };
+
+  const handleUndoCanvas = () => {
+    currentCanvas.current.undo();
+  };
+
+  const handleSendClick = () => {
+    alert('Sent!');
+  };
+
+  const handlePassAway = () => {
+    prompt('Are you sure?','yes');
+  };
+
   return (
-    <>
-      <CanvasDraw { ...canvas }/>
-    </>
+    <div className="bg-white w-full m-0 relative">
+      <CanvasDraw { ...canvas } ref={ currentCanvas }/>
+      <div id="controls" className="bg-slate-50 absolute bottom-0 h-28 pb-6 pt-3 px-3 w-full flex justify-between gap-2">
+        <button className="text-xl font-bold m-auto w-full h-full rounded-2xl transition-all ease-in duration-100 hover:animate-pulse bg-white hover:bg-rose-700 hover:text-white" onClick={ handleEraseCanvas }>
+          Erase
+        </button>
+        <button className="text-xl font-bold m-auto w-full h-full rounded-2xl transition-all ease-in duration-100 hover:animate-pulse bg-white hover:bg-amber-700 hover:text-white" onClick={ handleUndoCanvas }>
+          Undo
+        </button>
+        <button className="text-xl font-bold m-auto w-full h-full rounded-2xl transition-all ease-in duration-100 hover:animate-pulse bg-white hover:bg-green-700 hover:text-white" onClick={ handleSendClick }>
+         Send
+        </button>
+        <button className="text-xl font-bold m-auto w-full h-full rounded-2xl transition-all ease-in duration-100 hover:animate-pulse bg-white hover:bg-gray-900 hover:text-white" onClick={ handlePassAway }>
+         Pass away
+        </button>
+      </div>
+    </div>
   );
 };
--- src/components/Layouts/MainContainer.tsx
+++ src/components/Layouts/MainContainer.tsx
@@ -1,39 +1,68 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CanvasContainer } from '../Canvas/CanvasContainer';
 
 export const MainContainer = (): JSX.Element | null => {
 
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [word, setWord] = useState( () => {
 
-  const [width, setWidth] = useState<number>();
-  const [height, setHeight] = useState<number>();
+    const words = [ 'stunning', 'knowledge', 'paralyzing', 'maintenance', 'wood', 'better' ];
+    return words[ Math.floor(Math.random() * words.length) ].toLocaleUpperCase();
+  });
 
-  const props = {
-    width,
-    height
+
+  const [counter, setCounter] = useState(0);
+  const [maxTime] = useState(60);
+
+  useEffect(() => {
+
+    const timeout = setTimeout( () => {
+      setCounter(counter+1);
+    }, 1000 );
+
+    if( counter === maxTime ) {
+      alert('You lost!');
+      clearTimeout(timeout);
+    }    
+
+    // Cleaning
+    return () => clearTimeout(timeout);
+  }, [counter]);
+
+  const progressBarCalculation = (): number => {
+    return ( counter / maxTime ) * 100;
   };
-
-
-  const getContainerSize = () => {
-    if( containerRef.current !== null ) {
-      const width = containerRef.current.clientWidth;
-      setWidth(width);
-
-      const height = containerRef.current.clientHeight;
-      setHeight(height);
-    }
-  };
-
-  useLayoutEffect( () => {
-    window.addEventListener( 'resize', getContainerSize );
-  }, [ getContainerSize ] );
 
   return (
     <>
-      <h1 className="mb-6 drop-shadow-lg animate-[bounce_2s_infinite]">Fun game!</h1>
-      <div className="bg-slate-100 w-2/3 h-2/4 lg:w-500 lg:h-500 rounded-xl shadow-xl overflow-hidden
-      flex align-center justify-center" ref={containerRef}>
-        <CanvasContainer { ...props }/>
+      <h1 className="mb-3 drop-shadow-lg animate-[bounce_2s_infinite]">Let the AI hang-you... or not!</h1>
+      <h3 className="mb-2">Colaborate with the AI drawing a letter into the screen</h3>
+      <div className="max-w-screen-lg rounded-xl shadow-xl overflow-hidden flex align-center justify-center">
+        <div className="grid grid-flow-row-dense grid-cols-2 gap-0">
+          <CanvasContainer/>
+          <div className="bg-gray-50 col-span-1 border-1 flex flex-col justify-end">
+
+            {/* Hangman */}
+            <div className="hangman p-3">
+              <h4 className="italic text-sm">
+                Get ready for the next challenge!
+              </h4>
+            </div>
+
+            {/* Timer */}
+            <div className="w-full bg-gray-200">
+              <div className="bg-sky-400 text-xs font-bold text-white p-1 leading-none transition-all duration-500 text-right" style={{ width: `${progressBarCalculation()}%` }}>{ counter } s</div>
+            </div>
+
+            {/* Letters */}
+            <div className="h-28 pb-6 pt-3 px-3 flex items-end gap-2">
+              {
+                !! word.length && word.split('').map( ( letter: string, index: number ) => (
+                  <div key={ `letter-${ index }`} className="outline-none rounded-2xl w-full h-full text-center text-4xl font-extrabold bg-white flex justify-center items-center">{ letter }</div> 
+                ))
+              }
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
